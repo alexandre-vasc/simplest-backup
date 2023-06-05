34729dea505444c91679a505555f0805887c4f2a
Alexandre Vasconcellos <avasconcellos204@gmail.com>
2023-06-05 09:09:53 -0300

    changing backup order. Encrypting after each zip

--- simplest-backup.sh
+++ simplest-backup.sh
@@ -1,6 +1,8 @@
-7zr u -up0q3r2x2y2z1w2  /dados/backup/documents.7z ~/Documents
 7zr u -up0q3r2x2y2z1w2  /dados/backup/firefox.7z ~/.mozilla/firefox/
+gpg --batch --passphrase-file key.txt --yes --output /dados/backup/firefox.7z.gpg --symmetric /dados/backup/firefox.7z
+
+7zr u -up0q3r2x2y2z1w2  /dados/backup/documents.7z ~/Documents
+gpg --batch --passphrase-file key.txt --yes --output /dados/backup/documents.7z.gpg --symmetric /dados/backup/documents.7z
+
 7zr u -up0q3r2x2y2z1w2  /dados/backup/chromium.7z ~/.config/chromium/Default/
 gpg --batch --passphrase-file key.txt --yes --output /dados/backup/chromium.7z.gpg --symmetric /dados/backup/chromium.7z
-gpg --batch --passphrase-file key.txt --yes --output /dados/backup/documents.7z.gpg --symmetric /dados/backup/documents.7z
-gpg --batch --passphrase-file key.txt --yes --output /dados/backup/firefox.7z.gpg --symmetric /dados/backup/firefox.7z
